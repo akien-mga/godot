b2c5425bb5dee242f358d13ab9457f775e96acf7
Rémi Verschelde <rverschelde@gmail.com>
2023-01-19 16:02:22 +0100

    astcenc: Compress first, then decompress :)
    Just for consistency with the header and for logical ordering.
    Will squash before merge.

--- modules/astcenc/image_compress_astcenc.cpp
+++ modules/astcenc/image_compress_astcenc.cpp
@@ -35,110 +35,6 @@
 
 #include <astcenc.h>
 
-void _decompress_astc(Image *r_img) {
-	uint64_t start_time = OS::get_singleton()->get_ticks_msec();
-
-	// Determine decompression parameters from image format.
-
-	Image::Format img_format = r_img->get_format();
-	bool is_hdr = false;
-	unsigned int block_x = 0;
-	unsigned int block_y = 0;
-	if (img_format == Image::FORMAT_ASTC_4x4) {
-		block_x = 4;
-		block_y = 4;
-		is_hdr = false;
-	} else if (img_format == Image::FORMAT_ASTC_4x4_HDR) {
-		block_x = 4;
-		block_y = 4;
-		is_hdr = true;
-	} else if (img_format == Image::FORMAT_ASTC_8x8) {
-		block_x = 8;
-		block_y = 8;
-		is_hdr = false;
-	} else if (img_format == Image::FORMAT_ASTC_8x8_HDR) {
-		block_x = 8;
-		block_y = 8;
-		is_hdr = true;
-	} else {
-		ERR_FAIL_MSG("astcenc: Cannot decompress Image with a non-ASTC format.");
-	}
-
-	// Initialize astcenc.
-
-	astcenc_profile profile = ASTCENC_PRF_LDR;
-	if (is_hdr) {
-		profile = ASTCENC_PRF_HDR;
-	}
-	astcenc_config config;
-	const float quality = ASTCENC_PRE_MEDIUM;
-
-	astcenc_error status = astcenc_config_init(profile, block_x, block_y, block_x, quality, 0, &config);
-	ERR_FAIL_COND_MSG(status != ASTCENC_SUCCESS,
-			vformat("astcenc: Configuration initialization failed: %s.", astcenc_get_error_string(status)));
-
-	// Context allocation.
-
-	astcenc_context *context = nullptr;
-	const unsigned int thread_count = OS::get_singleton()->get_processor_count();
-
-	status = astcenc_context_alloc(&config, thread_count, &context);
-	ERR_FAIL_COND_MSG(status != ASTCENC_SUCCESS,
-			vformat("astcenc: Context allocation failed: %s.", astcenc_get_error_string(status)));
-
-	// Decompress image.
-
-	const bool mipmaps = r_img->has_mipmaps();
-	int width = r_img->get_width();
-	int height = r_img->get_height();
-
-	astcenc_image image;
-	image.dim_x = width;
-	image.dim_y = height;
-	image.dim_z = 1;
-	image.data_type = ASTCENC_TYPE_U8;
-	Image::Format target_format = Image::FORMAT_RGBA8;
-	if (is_hdr) {
-		target_format = Image::FORMAT_RGBAF;
-		image.data_type = ASTCENC_TYPE_F32;
-	}
-
-	Vector<uint8_t> image_data = r_img->get_data();
-
-	Vector<uint8_t> new_image_data;
-	new_image_data.resize(Image::get_image_data_size(width, height, target_format, false));
-	new_image_data.fill(0);
-	uint8_t *slices = new_image_data.ptrw();
-	image.data = reinterpret_cast<void **>(&slices);
-
-	const astcenc_swizzle swizzle = {
-		ASTCENC_SWZ_R, ASTCENC_SWZ_G, ASTCENC_SWZ_B, ASTCENC_SWZ_A
-	};
-
-	status = astcenc_decompress_image(context, image_data.ptr(), image_data.size(), &image, &swizzle, 0);
-	ERR_FAIL_COND_MSG(status != ASTCENC_SUCCESS,
-			vformat("astcenc: ASTC decompression failed: %s.", astcenc_get_error_string(status)));
-	ERR_FAIL_COND_MSG(image.dim_z > 1,
-			"astcenc: ASTC decompression failed because this is a 3D texture, which is not supported.");
-
-	// Replace original image with compressed one.
-
-	Image::Format image_format = Image::FORMAT_RGBA8;
-	if (image.data_type == ASTCENC_TYPE_F32) {
-		image_format = Image::FORMAT_RGBAF;
-	} else if (image.data_type == ASTCENC_TYPE_U8) {
-		image_format = Image::FORMAT_RGBA8;
-	} else if (image.data_type == ASTCENC_TYPE_F16) {
-		image_format = Image::FORMAT_RGBAH;
-	} else {
-		ERR_FAIL_MSG("astcenc: ASTC decompression failed with an unknown format.");
-	}
-
-	r_img->set_data(image.dim_x, image.dim_y, mipmaps, image_format, new_image_data);
-
-	print_verbose(vformat("astcenc: Decompression took %s ms.", rtos(OS::get_singleton()->get_ticks_msec() - start_time)));
-}
-
 void _compress_astc(Image *r_img, float p_lossy_quality, Image::ASTCFormat p_format) {
 	uint64_t start_time = OS::get_singleton()->get_ticks_msec();
 
@@ -248,4 +144,108 @@ void _compress_astc(Image *r_img, float p_lossy_quality, Image::ASTCFormat p_for
 	r_img->set_data(width, height, mipmaps, target_format, compressed_data);
 
 	print_verbose(vformat("astcenc: Encoding took %s ms.", rtos(OS::get_singleton()->get_ticks_msec() - start_time)));
+}
+
+void _decompress_astc(Image *r_img) {
+	uint64_t start_time = OS::get_singleton()->get_ticks_msec();
+
+	// Determine decompression parameters from image format.
+
+	Image::Format img_format = r_img->get_format();
+	bool is_hdr = false;
+	unsigned int block_x = 0;
+	unsigned int block_y = 0;
+	if (img_format == Image::FORMAT_ASTC_4x4) {
+		block_x = 4;
+		block_y = 4;
+		is_hdr = false;
+	} else if (img_format == Image::FORMAT_ASTC_4x4_HDR) {
+		block_x = 4;
+		block_y = 4;
+		is_hdr = true;
+	} else if (img_format == Image::FORMAT_ASTC_8x8) {
+		block_x = 8;
+		block_y = 8;
+		is_hdr = false;
+	} else if (img_format == Image::FORMAT_ASTC_8x8_HDR) {
+		block_x = 8;
+		block_y = 8;
+		is_hdr = true;
+	} else {
+		ERR_FAIL_MSG("astcenc: Cannot decompress Image with a non-ASTC format.");
+	}
+
+	// Initialize astcenc.
+
+	astcenc_profile profile = ASTCENC_PRF_LDR;
+	if (is_hdr) {
+		profile = ASTCENC_PRF_HDR;
+	}
+	astcenc_config config;
+	const float quality = ASTCENC_PRE_MEDIUM;
+
+	astcenc_error status = astcenc_config_init(profile, block_x, block_y, block_x, quality, 0, &config);
+	ERR_FAIL_COND_MSG(status != ASTCENC_SUCCESS,
+			vformat("astcenc: Configuration initialization failed: %s.", astcenc_get_error_string(status)));
+
+	// Context allocation.
+
+	astcenc_context *context = nullptr;
+	const unsigned int thread_count = OS::get_singleton()->get_processor_count();
+
+	status = astcenc_context_alloc(&config, thread_count, &context);
+	ERR_FAIL_COND_MSG(status != ASTCENC_SUCCESS,
+			vformat("astcenc: Context allocation failed: %s.", astcenc_get_error_string(status)));
+
+	// Decompress image.
+
+	const bool mipmaps = r_img->has_mipmaps();
+	int width = r_img->get_width();
+	int height = r_img->get_height();
+
+	astcenc_image image;
+	image.dim_x = width;
+	image.dim_y = height;
+	image.dim_z = 1;
+	image.data_type = ASTCENC_TYPE_U8;
+	Image::Format target_format = Image::FORMAT_RGBA8;
+	if (is_hdr) {
+		target_format = Image::FORMAT_RGBAF;
+		image.data_type = ASTCENC_TYPE_F32;
+	}
+
+	Vector<uint8_t> image_data = r_img->get_data();
+
+	Vector<uint8_t> new_image_data;
+	new_image_data.resize(Image::get_image_data_size(width, height, target_format, false));
+	new_image_data.fill(0);
+	uint8_t *slices = new_image_data.ptrw();
+	image.data = reinterpret_cast<void **>(&slices);
+
+	const astcenc_swizzle swizzle = {
+		ASTCENC_SWZ_R, ASTCENC_SWZ_G, ASTCENC_SWZ_B, ASTCENC_SWZ_A
+	};
+
+	status = astcenc_decompress_image(context, image_data.ptr(), image_data.size(), &image, &swizzle, 0);
+	ERR_FAIL_COND_MSG(status != ASTCENC_SUCCESS,
+			vformat("astcenc: ASTC decompression failed: %s.", astcenc_get_error_string(status)));
+	ERR_FAIL_COND_MSG(image.dim_z > 1,
+			"astcenc: ASTC decompression failed because this is a 3D texture, which is not supported.");
+
+	// Replace original image with compressed one.
+
+	Image::Format image_format = Image::FORMAT_RGBA8;
+	if (image.data_type == ASTCENC_TYPE_F32) {
+		image_format = Image::FORMAT_RGBAF;
+	} else if (image.data_type == ASTCENC_TYPE_U8) {
+		image_format = Image::FORMAT_RGBA8;
+	} else if (image.data_type == ASTCENC_TYPE_F16) {
+		image_format = Image::FORMAT_RGBAH;
+	} else {
+		ERR_FAIL_MSG("astcenc: ASTC decompression failed with an unknown format.");
+	}
+
+	r_img->set_data(image.dim_x, image.dim_y, mipmaps, image_format, new_image_data);
+
+	print_verbose(vformat("astcenc: Decompression took %s ms.", rtos(OS::get_singleton()->get_ticks_msec() - start_time)));
 }
