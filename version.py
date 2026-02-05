69720f385ad3576b001e4cd3130d36ba06779d8b
Rémi Verschelde <remi@godotengine.org>
2026-02-05 21:39:49 +0100

    Bump version down to 4.6-dotnet

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "godot"
 name = "Godot Engine"
 major = 4
-minor = 7
+minor = 6
 patch = 0
-status = "dev"
+status = "dotnet"
 module_config = ""
 website = "https://godotengine.org"
-docs = "latest"
+docs = "4.6"
